9178384ce021016da300d492ac1995876dd7ba36
mircea bardan <bardan.mircea.calin@gmail.com>
2022-09-15 23:19:37 +0200

    update4

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : jeu. 15 sep. 2022 à 10:48
+-- Généré le : jeu. 15 sep. 2022 à 23:17
 -- Version du serveur : 10.4.24-MariaDB
 -- Version de PHP : 8.1.6
 
@@ -210,6 +210,73 @@ ALTER TABLE `produit`
 ALTER TABLE `produit`
   ADD CONSTRAINT `produit_ib_fk_2` FOREIGN KEY (`marque_id`) REFERENCES `marque` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`categorie_id`) REFERENCES `categorie` (`id`);
+--
+-- Base de données : `entreprise`
+--
+CREATE DATABASE IF NOT EXISTS `entreprise` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `entreprise`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `employes`
+--
+
+CREATE TABLE `employes` (
+  `id_employes` int(11) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `sexe` enum('m','f') NOT NULL,
+  `service` varchar(50) NOT NULL,
+  `date_embauche` date NOT NULL,
+  `salaire` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `employes`
+--
+
+INSERT INTO `employes` (`id_employes`, `prenom`, `nom`, `sexe`, `service`, `date_embauche`, `salaire`) VALUES
+(350, 'Jean-pierre', 'Laborde', 'm', 'direction', '2010-12-09', 5000),
+(388, 'Clement', 'Gallet', 'm', 'commercial', '2010-12-15', 2300),
+(415, 'Thomas', 'Winter', 'm', 'commercial', '2011-05-03', 3550),
+(417, 'Chloe', 'Dubar', 'f', 'production', '2011-09-05', 1900),
+(491, 'Elodie', 'Fellier', 'f', 'secretariat', '2011-11-22', 1600),
+(509, 'Fabrice', 'Grand', 'm', 'comptabilite', '2011-12-30', 2900),
+(547, 'Melanie', 'Collier', 'f', 'commercial', '2012-01-08', 3100),
+(592, 'Laura', 'Blanchet', 'f', 'direction', '2012-05-09', 4500),
+(627, 'Guillaume', 'Miller', 'm', 'commercial', '2012-07-02', 1900),
+(655, 'Celine', 'Perrin', 'f', 'commercial', '2012-09-10', 2700),
+(699, 'Julien', 'Cottet', 'm', 'secretariat', '2013-01-05', 1390),
+(701, 'Mathieu', 'Vignal', 'm', 'informatique', '2013-04-03', 2500),
+(739, 'Thierry', 'Desprez', 'm', 'secretariat', '2013-07-17', 1500),
+(780, 'Amandine', 'Thoyer', 'f', 'communication', '2014-01-23', 2100),
+(802, 'Damien', 'Durand', 'm', 'informatique', '2014-07-05', 2250),
+(854, 'Daniel', 'Chevel', 'm', 'informatique', '2015-09-28', 3100),
+(876, 'Nathalie', 'Martin', 'f', 'juridique', '2016-01-12', 3550),
+(900, 'Benoit', 'Lagarde', 'm', 'production', '2016-06-03', 2550),
+(933, 'Emilie', 'Sennard', 'f', 'commercial', '2017-01-11', 1800),
+(990, 'Stephanie', 'Lafaye', 'f', 'assistant', '2017-03-01', 1775);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `employes`
+--
+ALTER TABLE `employes`
+  ADD PRIMARY KEY (`id_employes`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `employes`
+--
+ALTER TABLE `employes`
+  MODIFY `id_employes` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=991;
 --
 -- Base de données : `phpmyadmin`
 --
@@ -700,6 +767,37 @@ CREATE TABLE `user` (
   `telephone` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `voiture`
+--
+
+CREATE TABLE `voiture` (
+  `id` int(11) NOT NULL,
+  `immatriculation` varchar(15) DEFAULT NULL,
+  `marque` varchar(50) NOT NULL,
+  `modele` varchar(50) NOT NULL,
+  `annee` int(4) NOT NULL,
+  `carburant` enum('essence','diesel','electrique','GPL') NOT NULL,
+  `type_vitesse` enum('automatique','manuelle') NOT NULL,
+  `nb_vitesse` int(1) NOT NULL,
+  `nb_porte` int(1) NOT NULL,
+  `couleur` varchar(30) NOT NULL,
+  `controle_technique` tinyint(1) NOT NULL,
+  `places` int(1) NOT NULL,
+  `type` enum('suv','cabriolet','citadine','4x4','sport','utilitaire') NOT NULL,
+  `prix` double NOT NULL,
+  `kilometrage` int(7) NOT NULL,
+  `nb_chevaux` int(4) NOT NULL,
+  `fiscaux` int(3) NOT NULL,
+  `etat` enum('neuve','tr?s bon ?tat','bon ?tat','moyen','en l''?tat','?pave','pour pi?ces') NOT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `option` text DEFAULT NULL,
+  `volant` enum('gauche','droite') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Index pour les tables déchargées
 --
@@ -711,6 +809,12 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `voiture`
+--
+ALTER TABLE `voiture`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
@@ -718,6 +822,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `voiture`
+--
+ALTER TABLE `voiture`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- Base de données : `test`
